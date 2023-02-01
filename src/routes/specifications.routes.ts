2176f49639c4e6ea5fb616ffbf52c48135304990
Murilo Thom <murilothom.2002@hotmail.com>
2023-02-01 14:39:43 -0300

    feat: create specification controller

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,9 @@
 import { Router } from 'express'
 
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
 
 export const specificationsRoutes = Router()
 
-const specificationsRepository = new SpecificationsRepository()
-
-specificationsRoutes.post('/', (req, res) => {
-  const { name, description } = req.body
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  )
-
-  createSpecificationService.execute({ name, description })
-
-  return res.status(201).send()
-})
+specificationsRoutes.post('/', (req, res) =>
+  createSpecificationController.handle(req, res)
+)

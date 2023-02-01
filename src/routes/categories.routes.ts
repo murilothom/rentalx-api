ad648a0b4f0e0866c7b5ca39b1f34990ce1b1113
Murilo Thom <murilothom.2002@hotmail.com>
2023-02-01 14:27:59 -0300

    feat: implementations

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,14 @@
 import { Router } from 'express'
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 export const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (req, res) =>
   createCategoryController.handle(req, res)
 )
 
-categoriesRoutes.get('/', (req, res) => {
-  const allCategories = categoriesRepository.list()
-
-  return res.json(allCategories)
-})
+categoriesRoutes.get('/', (req, res) =>
+  listCategoriesController.handle(req, res)
+)

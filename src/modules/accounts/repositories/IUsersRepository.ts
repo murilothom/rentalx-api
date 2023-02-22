6fd8482baa060649c0cf0a721d2eea1f3ca7e283
Murilo Thom <murilothom.2002@hotmail.com>
2023-02-22 18:28:07 -0300

    fix: imports

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,5 @@
-import { ICreateUserDTO } from '../dtos/ICreateUserDto'
-import { User } from '../entities/User'
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDto'
+import { User } from '@modules/accounts/entities/User'
 
 export interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>
--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -1,8 +1,8 @@
 import { getRepository, Repository } from 'typeorm'
 
-import { ICreateUserDTO } from '../../dtos/ICreateUserDto'
-import { User } from '../../entities/User'
-import { IUsersRepository } from '../IUsersRepository'
+import { ICreateUserDTO } from '@modules/accounts/dtos/ICreateUserDto'
+import { User } from '@modules/accounts/entities/User'
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
 
 export class UsersRepository implements IUsersRepository {
   private repository: Repository<User>
